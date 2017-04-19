cb3e72453ab6b7e83b5119dcc5e58f4eb7a9907e
Phi Huynh <phihuynh@nashtechglobal.com>
2017-04-19 23:50:58 +0700

    #10 Remove unneccessary echos

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ for HOST_NAME in ${HOST_NAMES[@]}
 do
     if grep $HOST_NAME /etc/hosts
     then
-        echo 'HOST_NAME EXISTS: ' $HOST_NAME
+        # echo 'HOST_NAME EXISTS: ' $HOST_NAME
         sed -i '' "/$HOST_NAME/ s/.*/$HOST_IP $HOST_NAME/g" /private/etc/hosts
     else
-        echo 'HOST_NAME DOESN'' EXIST: ' $HOST_NAME
+        # echo 'HOST_NAME DOESN'' EXIST: ' $HOST_NAME
         echo $HOST_IP $HOST_NAME >> /private/etc/hosts
     fi
 done
