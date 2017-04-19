c5d78e487c1b8fbd4f3c762cd34f1230c8ed8ddf
Phi Huynh <phihuynh@nashtechglobal.com>
2017-04-19 23:48:42 +0700

    #10 build.sh always adding a new record to hosts file #10

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 export HOST_IP=$(ifconfig en0 | grep inet | grep -v inet6 | cut -d ' ' -f2)
-echo $HOST_IP git.yourcompany.com | sudo tee -a /private/etc/hosts
-echo $HOST_IP ldap.yourcompany.com | sudo tee -a /private/etc/hosts
-echo $HOST_IP jenkins.yourcompany.com | sudo tee -a /private/etc/hosts
+
+HOST_NAMES[0]="git.yourcompany.com"
+HOST_NAMES[1]="ldap.yourcompany.com"
+HOST_NAMES[2]="jenkins.yourcompany.com"
+HOST_NAMES[4]="hub.yourcompany.com"
+
+for HOST_NAME in ${HOST_NAMES[@]}
+do
+    if grep $HOST_NAME /etc/hosts
+    then
+        echo 'HOST_NAME EXISTS: ' $HOST_NAME
+        sed -i '' "/$HOST_NAME/ s/.*/$HOST_IP $HOST_NAME/g" /private/etc/hosts
+    else
+        echo 'HOST_NAME DOESN'' EXIST: ' $HOST_NAME
+        echo $HOST_IP $HOST_NAME >> /private/etc/hosts
+    fi
+done
+
 docker-compose up
